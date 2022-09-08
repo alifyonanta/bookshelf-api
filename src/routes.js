1cda5715a30bbb081cb6de2637083a72f9b6dbc1
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2022-09-08 16:24:45 +0700

    api book

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addBookHandler, getAllBookHandler, getBookbyIdHandler, editBookbyIdHandler, deleteBookbyIdHandler } = require('./handler');
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookbyIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookbyIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookbyIdHandler,
+    },
+
+];
+
+module.exports = routes;
